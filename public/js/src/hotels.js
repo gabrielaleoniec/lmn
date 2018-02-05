904d0772023df0cf3a4a554c8f1e8223c2a25301
Gabriela Leoniec <gabriela.leoniec@gmail.com>
2018-02-05 22:58:22 +0100

    TDD for hotels. Testing for the JSON list of hotels

--- public/js/src/hotels.js
+++ public/js/src/hotels.js
@@ -3,26 +3,24 @@ class Hotels {
     this.list = null;
   }
 
-  getList(url) {
-    return new Promise(function(resolve, reject){
-			if(typeof url === 'string' && url.length){
-				let xhr = new window.XMLHttpRequest();
-				// It's enough to check the headers to know if an image exists
-				// Last parameter is for asynchronous request
-				// Could be done asynchronously with the usage of ES7 await
-				xhr.open('GET', url, false);
-				xhr.send();
-				if(xhr.status === 200 || xhr.status === 304) {
-          // TODO: check if it's a proper JSON
-          this.list = xhr.responseText;
-          resolve(xhr);
-				} else {
-					reject('Error in sending a request '+ url + ' status: '+xhr.status + ' message: '+xhr.statusText);
-				}
-			} else {
-				reject('checkImage: ' +url + ' is not a string');
-			}
-		}.bind(this));
+  getList(url, callback) {
+    if(typeof url === 'string' && url.length){
+      let xhr = new window.XMLHttpRequest();
+      // It's enough to check the headers to know if an image exists
+      // Last parameter is for asynchronous request
+      // Could be done asynchronously with the usage of ES7 await
+      xhr.open('GET', url, false);
+      xhr.send();
+      if(xhr.status === 200 || xhr.status === 304) {
+        // TODO: check if it's a proper JSON
+        this.list = xhr.responseText;
+        callback(null, JSON.parse(xhr.responseText));
+      } else {
+        callback(xhr.status);
+      }
+    } else {
+      throw new Error('getList: ' +url + ' is not a string');
+    }
   }
 
   setList() {
--- test/hotels-spec.js
+++ test/hotels-spec.js
@@ -17,45 +17,32 @@ describe('Hotels', () => {
   
   describe('#getList', () => {
     let hotels = new Hotels(),
-    server = 'http://localhost:8765', //'http://localhost:8765',
-        path = '/api/hotels',
-        url = server+path;
-        
-        var json = JSON.stringify([
-          {"id":"1","name":"Hotel Sunny Palms"},
-          {"id":"2","name":"Hotel Snowy Mountains"},
-          {"id":"3","name":"Hotel Windy Sails"},
-          {"id":"4","name":"Hotel Middle of Nowhere"}
-        ]),
-        wrong_json = JSON.stringify(
-          {"id":"1","name":"Hotel Sunny Palms"}
-        );
-    
-    beforeEach(()=>{
-        nock.disableNetConnect('http://localhost:8765'); 
-        nock.disableNetConnect('http://localhost');
-        nock(server)
-					.get(path)
-					.reply(404, wrong_json);
-      });
-      
-      afterEach(()=>{
-        nock.cleanAll();
-      });
-    
-    it('should fetch json with hotels from server', (done) => {
+      server = 'http://localhost:8765', //'http://localhost:8765',
+      path = '/api/hotels',
+      url = server+path;
+
+    var json = JSON.stringify([
+        {"id":"1","name":"Hotel Sunny Palms"},
+        {"id":"2","name":"Hotel Snowy Mountains"},
+        {"id":"3","name":"Hotel Windy Sails"},
+        {"id":"4","name":"Hotel Middle of Nowhere"}
+      ]),
+      wrong_json = JSON.stringify(
+        {"id":"1","name":"Hotel Sunny Palms"}
+      );
+       
+    it('should fetch json with hotels from server', function(done) {
       global.window = dom1.window;
       
       expect(hotels.list).to.be.null;
       expect(hotels.getList).to.be.a('function');
       
-			hotels.getList(url).then(() => {
-        expect(hotels.list).equals(wrong_json);
-        done();       
-			}).catch(err => console.error(err));
+			hotels.getList(url, function(err, result) {
+        expect(hotels.list).to.not.be.null;
+        expect(JSON.parse(hotels.list)).to.have.length(4);
+        expect(hotels.list).to.equal(json);
+        done();
+      });
     });
   });
-  
-  
-
 });
